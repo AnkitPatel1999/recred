80b4da783ff505feffd14f104c0c2e85fd7a0c61
AJTech1968 <40666123+AJTech1968@users.noreply.github.com>
2021-05-12 00:15:09 +0530

    product added

--- src/user/Products.js
+++ src/user/Products.js
@@ -1,10 +1,78 @@
 import React from 'react'
 
 const Products = () => {
+
+   
+    let Product = [
+        {
+            cardTitles:'this is title',
+            imageUrl: 'https://images.pexels.com/photos/125437/pexels-photo-125437.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500 1000w',
+            cardDescription:" this is descri[tion",
+            cardPrice: '2'
+        },
+       {
+            cardTitles:'title',
+            imageUrl: 'https://images.pexels.com/photos/667803/pexels-photo-667803.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500 1000w',
+            cardDescription:" thisis descritpion",
+            cardPrice: '130'
+        },
+        {
+            cardTitles:'title',
+            imageUrl: 'https://images.pexels.com/photos/707377/pexels-photo-707377.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500 1000w',
+            cardDescription:" thisis descritpion",
+            cardPrice: '140'
+        },
+       {
+            cardTitles:'title',
+            imageUrl: 'https://images.pexels.com/photos/33999/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500 500w,',
+            cardDescription:" thisis descritpion",
+            cardPrice: '150'
+        },
+        {
+            cardTitles:'title',
+            imageUrl: 'https://images.pexels.com/photos/122436/girl-water-wet-summer-122436.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 500w',
+            cardDescription:" thisis descritpion",
+            cardPrice: '160'
+        },
+       {
+            cardTitles:'title',
+            imageUrl: 'https://images.pexels.com/photos/114011/pexels-photo-114011.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 500w',
+            cardDescription:" thisis descritpion",
+            cardPrice: '120'
+        },
+    ]
+
+    
+
     return (
-        <div>
-            <h1 className="text-center">Hey thats products</h1>
-        </div>
+        <div className="row">
+        {Product.map((product, index)=> (
+            <div className="col-4 mt-2">
+                <div className="card text-white bg-dark border border-info">
+                    <div className="card-header lead">{product.cardTitles}</div>
+                        <div className="card-body">
+                            <div className="rounded border border-success p-2">
+                                <img src={ product.imageUrl }
+                                    alt="photo"
+                                    style={{maxHeight: "100%", maxWidth: "100%"}}
+                                    className="mb-3 rounded"
+                                />
+                            </div>
+                            <p className="lead bg-success font-weight-normal text-wrap">{ product.cardDescription}</p>
+                            <p className="btn btn-success rounded btn-sm px-4">$ { product.cardPrice }</p>
+                            <div className="row">
+                                <div className="col-12">
+                                    showAddToCard
+                                </div>
+                                <div className="col-12">
+                                    showRemoveFromCard
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        ))}
+        </div>  
     )
 }
 
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,15 +1,19 @@
 import React,{ useState, useEffect } from 'react'
 import '../styles.css'
-import {Redirect} from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
+import Products from '../user/Products'
 
 const Signup = () => {
 
     const [values, setValues] = useState({
         name: '',
-        email: ''
+        email: '',
+        pic: 'https://www.pexels.com/photo/woman-in-white-dress-shirt-holding-yellow-flowers-7532824/',
     })
-    const {name , email} = values;
+    const {name , email, pic} = values;
+
+    const [userValue, setUserValue] = useState(false)
+
 
     const insertGapiScript = () =>{
         const script = document.createElement('script')
@@ -18,16 +22,15 @@ const Signup = () => {
             initalizeGoogleSignIn()
         }
         document.body.appendChild(script)
-        return <Redirect to="/products" />
     }
 
-    
+    const initalizeGoogleSignIn = () => {
 
-    const initalizeGoogleSignIn= () => {
         window.gapi.load('auth2', () => {
             window.gapi.auth2.init({
                 client_id: '320385499802-nfe8i2bdqop000fjomde5tcg8muv3rkv.apps.googleusercontent.com',
             })
+            
             console.log("api inited");
             window.gapi.load('signin2', () => {
                 const params = {
@@ -37,8 +40,9 @@ const Signup = () => {
                             console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
                             console.log('Image URL: ' + profile.getImageUrl());
                             console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-                            setValues({name: profile.getName(), email: profile.getEmail()})
-                           
+
+                            setValues({name: profile.getName(), email: profile.getEmail(), pic: profile.getImageUrl()})
+                            setUserValue(true)                   
                     }
                 }
                 
@@ -48,9 +52,10 @@ const Signup = () => {
     }
 
     useEffect(() => {
-        console.log('loading');
+        console.log('loading',userValue);
         insertGapiScript();
-    }, [])
+       
+    }, [userValue])
 
     const logout = () => {
         console.log('in logout');
@@ -58,16 +63,68 @@ const Signup = () => {
         let auth2 = window.gapi && window.gapi.auth2.getAuthInstance();
         auth2.signOut().then(function () {
             setValues({name: undefined, email: undefined})
+            setUserValue(false)
             console.log('User signed out.');
           });
     }
-        
+
+    const navBar = () => {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-9 bg-dark p-0">
+                        <ul className="nav nav-tabs bg-dark">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/cart">Cart</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-3 p-0">
+                        <ul className="nav nav-tabs bg-dark">
+                            { userValue && (
+                                <img src={ pic }
+                                    alt="photo"
+                                    style={{maxHeight: "100%", maxWidth: "12%"}}
+                                    className="roundeds"
+                                />
+                            )}
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">{ name } |</Link>
+                            </li>
+                            
+                            { userValue && (
+                                <li className="nav-item">
+                                    <span className="nav-link" onClick={logout}>Logout</span>
+                                </li> 
+                            )}
+                            
+                        </ul>
+                    </div>
+                </div>   
+            </div>
+        )
+    }
+
     return (
         <div>
-            <h1>Google singin </h1>
-            <div id="loginButton">Sign in with Google </div>
-            <h3>{name}{email}</h3>
-            <button onClick={logout}>Logout</button>
+            {navBar()}
+            { !userValue && (
+                <div className="row">
+                    <div className="col-5"></div>
+                    <div className="col-2">
+                        <div id="loginButton"> </div>
+                    </div>
+                    <div className="col-5"></div>
+                </div>
+            )}
+            { userValue &&(
+                <div>
+                    <Products />
+                </div>
+            )}
         </div>
     )
 }
